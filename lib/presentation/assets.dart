942089222838751feb6916bdce5fc5d80db0a5ba
Daniel Górka <daniel.gorka24@gmail.com>
2022-10-20 18:02:03 +0200

    Add beer_loading.riv to assets

--- lib/presentation/assets.dart
+++ lib/presentation/assets.dart
@@ -1,4 +1,5 @@
 abstract class Assets {
+  static const beerLoading = 'assets/beer_loading.riv';
   static const favMug = 'assets/fav-mug.svg';
   static const favMugColoredBlack = 'assets/fav-mug-colored-black.svg';
   static const favMugColoredWhite = 'assets/fav-mug-colored-white.svg';
