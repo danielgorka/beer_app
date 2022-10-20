4fd8ea524520fbb547c35b73bdd46b0abbb8befe
Daniel Górka <daniel.gorka24@gmail.com>
2022-10-20 15:09:30 +0200

    Add mug icons to assets

---
+++ lib/presentation/assets.dart
@@ -0,0 +1,8 @@
+abstract class Assets {
+  static const favMug = 'assets/fav-mug.svg';
+  static const favMugColoredBlack = 'assets/fav-mug-colored-black.svg';
+  static const favMugColoredWhite = 'assets/fav-mug-colored-white.svg';
+  static const favMugFilled = 'assets/fav-mug-filled.svg';
+  static const mug = 'assets/mug.svg';
+  static const mugFilled = 'assets/mug-filled.svg';
+}
