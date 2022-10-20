7997045cc808b098d95956a9cfef0503c51c5920
Daniel Górka <daniel.gorka24@gmail.com>
2022-10-20 15:22:47 +0200

    Create HomePage

--- lib/presentation/pages/core/home_page.dart
+++ lib/presentation/pages/core/home_page.dart
@@ -1,10 +1,74 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:beer_app/l10n/l10n.dart';
+import 'package:beer_app/presentation/assets.dart';
+import 'package:beer_app/presentation/routes/router.dart';
+import 'package:beer_app/presentation/utils.dart';
+import 'package:beer_app/presentation/widgets/asset_icon.dart';
+import 'package:beer_app/presentation/widgets/navigation_scaffold.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({
+    super.key,
+    this.beersRouter = const BeersRouter(),
+    this.favouritesBeersRouter = const FavouritesRouter(),
+  });
+
+  final PageRouteInfo beersRouter;
+  final PageRouteInfo favouritesBeersRouter;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return WillPopScope(
+      onWillPop: () => Future.value(false),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return NavigationScaffold(
+            navigationType: _getNavigationType(constraints),
+            appBar: AppBar(
+              title: Text(context.l10n.appName),
+            ),
+            headerBuilder: (context, animation) {
+              return FadeTransition(
+                opacity: animation,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 64, 16, 16),
+                  child: Text(
+                    context.l10n.appName,
+                    style: Theme.of(context).textTheme.headlineSmall,
+                    overflow: TextOverflow.fade,
+                    maxLines: 1,
+                  ),
+                ),
+              );
+            },
+            items: [
+              NavigationItem(
+                label: context.l10n.beers,
+                icon: const AssetIcon(Assets.mug),
+                activeIcon: const AssetIcon(Assets.mugFilled),
+                route: beersRouter,
+              ),
+              NavigationItem(
+                label: context.l10n.favourites,
+                icon: const AssetIcon(Assets.favMug),
+                activeIcon: const AssetIcon(Assets.favMugFilled),
+                route: favouritesBeersRouter,
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  NavigationType _getNavigationType(BoxConstraints constraints) {
+    if (DisplaySize.isLarge(constraints)) {
+      return NavigationType.drawer;
+    } else if (DisplaySize.isMedium(constraints)) {
+      return NavigationType.navRail;
+    } else {
+      return NavigationType.bottomNav;
+    }
   }
 }
---
+++ test/presentation/pages/core/home_page_test.dart
@@ -0,0 +1,149 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:beer_app/l10n/l10n.dart';
+import 'package:beer_app/presentation/pages/core/home_page.dart';
+import 'package:beer_app/presentation/routes/router.dart';
+import 'package:beer_app/presentation/widgets/navigation_scaffold.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../utils/mockables.dart';
+import '../../../utils/widget_tester_ext.dart';
+
+void main() {
+  group(
+    'HomePage',
+    () {
+      late AppRouter mockAppRouter;
+
+      setUpAll(() {
+        registerFallbackValue(const PageRouteInfo<void>('', path: ''));
+      });
+
+      setUp(() {
+        mockAppRouter = MockAppRouter();
+
+        when(() => mockAppRouter.navigate(any()))
+            .thenAnswer((_) => Future<void>.value());
+        when(() => mockAppRouter.navigateNamed(any()))
+            .thenAnswer((_) => Future.value());
+      });
+
+      Future<void> pumpWidget(WidgetTester tester) {
+        return tester.pumpAppWidget(
+          RouteDataScope(
+            routeData: RouteData(
+              route: const RouteMatch<dynamic>(
+                name: MockStackRouter.main,
+                segments: [],
+                path: '/',
+                stringMatch: '/',
+                key: ValueKey(MockStackRouter.main),
+              ),
+              router: MockStackRouter(),
+              pendingChildren: [],
+            ),
+            child: RouterScope(
+              stateHash: 0,
+              inheritableObserversBuilder: () => [],
+              navigatorObservers: const [],
+              controller: MockStackRouter(),
+              child: StackRouterScope(
+                controller: mockAppRouter,
+                stateHash: 0,
+                child: const HomePage(
+                  beersRouter: PageRouteInfo<void>(
+                    MockStackRouter.tab1,
+                    path: MockStackRouter.tab1,
+                  ),
+                  favouritesBeersRouter: PageRouteInfo<void>(
+                    MockStackRouter.tab2,
+                    path: MockStackRouter.tab2,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      }
+
+      testWidgets(
+        'should onWillPop returns false',
+        (tester) async {
+          // act
+          await pumpWidget(tester);
+
+          final finder = find.byType(WillPopScope);
+          final willPopScope = tester.widget<WillPopScope>(finder);
+          final result = await willPopScope.onWillPop!();
+
+          // assert
+          expect(result, isFalse);
+        },
+      );
+
+      testWidgets(
+        'should show app name',
+        (tester) async {
+          // act
+          await pumpWidget(tester);
+
+          // assert
+          final context = tester.element(find.byType(HomePage));
+          expect(find.text(context.l10n.appName), findsOneWidget);
+        },
+      );
+
+      testWidgets(
+        'should show NavigationScaffold with NavigationType.bottomNav '
+        'when device is small',
+        (tester) async {
+          // arrange
+          tester.setSmallDisplaySize();
+
+          // act
+          await pumpWidget(tester);
+
+          // assert
+          final finder = find.byType(NavigationScaffold);
+          final navigationScaffold = tester.widget<NavigationScaffold>(finder);
+          expect(navigationScaffold.navigationType, NavigationType.bottomNav);
+        },
+      );
+
+      testWidgets(
+        'should show NavigationScaffold with NavigationType.navRail '
+        'when device is medium',
+        (tester) async {
+          // arrange
+          tester.setMediumDisplaySize();
+
+          // act
+          await pumpWidget(tester);
+
+          // assert
+          final finder = find.byType(NavigationScaffold);
+          final navigationScaffold = tester.widget<NavigationScaffold>(finder);
+          expect(navigationScaffold.navigationType, NavigationType.navRail);
+        },
+      );
+
+      testWidgets(
+        'should show NavigationScaffold with NavigationType.drawer '
+        'when device is large',
+        (tester) async {
+          // arrange
+          tester.setLargeDisplaySize();
+
+          // act
+          await pumpWidget(tester);
+
+          // assert
+          final finder = find.byType(NavigationScaffold);
+          final navigationScaffold = tester.widget<NavigationScaffold>(finder);
+          expect(navigationScaffold.navigationType, NavigationType.drawer);
+        },
+      );
+    },
+  );
+}
