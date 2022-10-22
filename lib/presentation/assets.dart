06f0ce2770a896f1231aa1d9eb473a13c87bf329
Daniel Górka <daniel.gorka24@gmail.com>
2022-10-22 13:34:07 +0200

    Remove fav-mug-colored black/white and add brown two tone version of this icon

--- lib/presentation/assets.dart
+++ lib/presentation/assets.dart
@@ -2,8 +2,7 @@ abstract class Assets {
   static const beerLoading = 'assets/beer_loading.riv';
   static const bottle = 'assets/bottle.svg';
   static const favMug = 'assets/fav-mug.svg';
-  static const favMugColoredBlack = 'assets/fav-mug-colored-black.svg';
-  static const favMugColoredWhite = 'assets/fav-mug-colored-white.svg';
+  static const favMugColored = 'assets/fav-mug-colored.svg';
   static const favMugFilled = 'assets/fav-mug-filled.svg';
   static const mug = 'assets/mug.svg';
   static const mugFilled = 'assets/mug-filled.svg';
