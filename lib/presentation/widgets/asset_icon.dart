9b32b430f10c3f5521d5f44abe03d15fea4f4863
Daniel Górka <daniel.gorka24@gmail.com>
2022-10-22 13:41:59 +0200

    Add size, color and isTwoTone properties in AssetIcon

--- lib/presentation/widgets/asset_icon.dart
+++ lib/presentation/widgets/asset_icon.dart
@@ -5,18 +5,27 @@ class AssetIcon extends StatelessWidget {
   const AssetIcon(
     this.asset, {
     super.key,
-  });
+    this.color,
+    this.size,
+    this.isTwoTone = false,
+  }) : assert(
+          !isTwoTone || color == null,
+          'Cannot set color when icon is two tone',
+        );
 
   final String asset;
+  final double? size;
+  final Color? color;
+  final bool isTwoTone;
 
   @override
   Widget build(BuildContext context) {
     final iconTheme = IconTheme.of(context);
     return SvgPicture.asset(
       asset,
-      width: iconTheme.size,
-      height: iconTheme.size,
-      color: iconTheme.color,
+      width: size ?? iconTheme.size,
+      height: size ?? iconTheme.size,
+      color: isTwoTone ? null : (color ?? iconTheme.color),
     );
   }
 }
--- test/presentation/widgets/asset_icon_test.dart
+++ test/presentation/widgets/asset_icon_test.dart
@@ -34,10 +34,69 @@ void main() {
               svgPicture.pictureProvider as ExactAssetPicture;
           expect(exactAssetPicture.assetName, asset);
           expect(svgPicture.width, iconTheme.size);
+          expect(svgPicture.height, iconTheme.size);
           expect(
             svgPicture.colorFilter,
             ColorFilter.mode(iconTheme.color!, BlendMode.srcIn),
           );
+        },
+      );
+
+      testWidgets(
+        'should show SvgPicture.asset with custom size and color',
+        (tester) async {
+          // arrange
+          const asset = Assets.favMug;
+          const size = 10.0;
+          const color = Colors.red;
+
+          // act
+          await tester.pumpAppWidget(
+            const AssetIcon(
+              asset,
+              size: size,
+              color: color,
+            ),
+          );
+
+          // assert
+          final svgPicture = tester.widget<SvgPicture>(find.byType(SvgPicture));
+          final exactAssetPicture =
+              svgPicture.pictureProvider as ExactAssetPicture;
+          expect(exactAssetPicture.assetName, asset);
+          expect(svgPicture.width, size);
+          expect(svgPicture.height, size);
+          expect(
+            svgPicture.colorFilter,
+            const ColorFilter.mode(color, BlendMode.srcIn),
+          );
+        },
+      );
+
+      testWidgets(
+        'should show SvgPicture.asset with no color filter '
+        'when isTwoTone is true',
+        (tester) async {
+          // arrange
+          const asset = Assets.favMug;
+          const iconTheme = IconThemeData(
+            color: Colors.red,
+          );
+
+          // act
+          await tester.pumpAppWidget(
+            const IconTheme(
+              data: iconTheme,
+              child: AssetIcon(
+                asset,
+                isTwoTone: true,
+              ),
+            ),
+          );
+
+          // assert
+          final svgPicture = tester.widget<SvgPicture>(find.byType(SvgPicture));
+          expect(svgPicture.colorFilter, null);
         },
       );
     },
