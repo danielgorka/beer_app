34f49660fb91e0511ceb8620cb2be0eb536821ca
Daniel Górka <daniel.gorka24@gmail.com>
2022-10-22 14:44:19 +0200

    Create beers utils

---
+++ lib/presentation/pages/beers/utils.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const beersGridViewDelegate = SliverGridDelegateWithMaxCrossAxisExtent(
+  maxCrossAxisExtent: 200,
+  mainAxisSpacing: 8,
+  mainAxisExtent: 325,
+);
+
+const beersViewPadding = EdgeInsets.all(16);
