ef494836add9888e374877fe954ce62863923389
Daniel Górka <daniel.gorka24@gmail.com>
2022-10-24 13:09:37 +0200

    Create SpecificationCard

---
+++ lib/presentation/pages/beer_details/cards/specification_card.dart
@@ -0,0 +1,56 @@
+import 'package:beer_app/domain/beers/models/beer.dart';
+import 'package:beer_app/l10n/l10n.dart';
+import 'package:beer_app/presentation/pages/beer_details/cards/details_card.dart';
+import 'package:beer_app/presentation/pages/beer_details/widgets/specification_tile.dart';
+import 'package:flutter/material.dart';
+
+class SpecificationCard extends StatelessWidget {
+  const SpecificationCard({
+    super.key,
+    required this.beer,
+  });
+
+  final Beer beer;
+
+  @override
+  Widget build(BuildContext context) {
+    return DetailsCard(
+      title: context.l10n.specification,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SpecificationTile(
+            title: context.l10n.abv,
+            tooltip: context.l10n.abvText,
+            helpUrl: context.l10n.abvUrl,
+            value: '${beer.abv.value}%',
+          ),
+          SpecificationTile(
+            title: context.l10n.ibu,
+            tooltip: context.l10n.ibuText,
+            helpUrl: context.l10n.ibuUrl,
+            value: beer.ibu?.value.toString(),
+          ),
+          SpecificationTile(
+            title: context.l10n.ebc,
+            tooltip: context.l10n.ebcText,
+            helpUrl: context.l10n.ebcUrl,
+            value: beer.ebc?.value.toString(),
+          ),
+          SpecificationTile(
+            title: context.l10n.srm,
+            tooltip: context.l10n.srmText,
+            helpUrl: context.l10n.srmUrl,
+            value: beer.srm?.value.toString(),
+          ),
+          SpecificationTile(
+            title: context.l10n.ph,
+            tooltip: context.l10n.phText,
+            helpUrl: context.l10n.phUrl,
+            value: beer.ph?.value.toString(),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ test/presentation/pages/beer_details/cards/specification_card_test.dart
@@ -0,0 +1,117 @@
+import 'package:beer_app/l10n/l10n.dart';
+import 'package:beer_app/presentation/pages/beer_details/cards/details_card.dart';
+import 'package:beer_app/presentation/pages/beer_details/cards/specification_card.dart';
+import 'package:beer_app/presentation/pages/beer_details/widgets/specification_tile.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../utils/mock_values/beer.dart';
+import '../../../../utils/widget_tester_ext.dart';
+
+void main() {
+  group(
+    'SpecificationCard',
+    () {
+      testWidgets(
+        'should show DetailsCard, specification title and 5 SpecificationTiles',
+        (tester) async {
+          // act
+          await tester.pumpAppWidget(
+            SpecificationCard(
+              beer: beer,
+            ),
+          );
+
+          // assert
+          expect(find.byType(DetailsCard), findsOneWidget);
+          final context = tester.element(find.byType(SpecificationCard));
+          expect(find.text(context.l10n.specification), findsOneWidget);
+          expect(find.byType(SpecificationTile), findsNWidgets(5));
+        },
+      );
+
+      testWidgets(
+        'should show SpecificationTile for ABV',
+        (tester) async {
+          // act
+          await tester.pumpAppWidget(
+            SpecificationCard(
+              beer: beer,
+            ),
+          );
+
+          // assert
+          final context = tester.element(find.byType(SpecificationCard));
+          expect(find.text(context.l10n.abv), findsOneWidget);
+          expect(find.text('${beer.abv.value}%'), findsOneWidget);
+        },
+      );
+
+      testWidgets(
+        'should show SpecificationTile for IBU',
+        (tester) async {
+          // act
+          await tester.pumpAppWidget(
+            SpecificationCard(
+              beer: beer,
+            ),
+          );
+
+          // assert
+          final context = tester.element(find.byType(SpecificationCard));
+          expect(find.text(context.l10n.ibu), findsOneWidget);
+          expect(find.text('${beer.ibu!.value}'), findsOneWidget);
+        },
+      );
+
+      testWidgets(
+        'should show SpecificationTile for EBC',
+        (tester) async {
+          // act
+          await tester.pumpAppWidget(
+            SpecificationCard(
+              beer: beer,
+            ),
+          );
+
+          // assert
+          final context = tester.element(find.byType(SpecificationCard));
+          expect(find.text(context.l10n.ebc), findsOneWidget);
+          expect(find.text('${beer.ebc!.value}'), findsOneWidget);
+        },
+      );
+      testWidgets(
+        'should show SpecificationTile for SRM',
+        (tester) async {
+          // act
+          await tester.pumpAppWidget(
+            SpecificationCard(
+              beer: beer,
+            ),
+          );
+
+          // assert
+          final context = tester.element(find.byType(SpecificationCard));
+          expect(find.text(context.l10n.srm), findsOneWidget);
+          expect(find.text('${beer.srm!.value}'), findsOneWidget);
+        },
+      );
+
+      testWidgets(
+        'should show SpecificationTile for pH',
+        (tester) async {
+          // act
+          await tester.pumpAppWidget(
+            SpecificationCard(
+              beer: beer,
+            ),
+          );
+
+          // assert
+          final context = tester.element(find.byType(SpecificationCard));
+          expect(find.text(context.l10n.ph), findsOneWidget);
+          expect(find.text('${beer.ph!.value}'), findsOneWidget);
+        },
+      );
+    },
+  );
+}
