1b1162f65466d481f03d49c6673e127cb6b22832
Daniel Górka <daniel.gorka24@gmail.com>
2022-10-25 11:32:51 +0200

    Create SliverSearchBar

---
+++ lib/presentation/widgets/sliver_search_bar.dart
@@ -0,0 +1,172 @@
+import 'package:beer_app/presentation/app_icons.dart';
+import 'package:beer_app/presentation/utils.dart';
+import 'package:beer_app/presentation/widgets/animated_fade_scale.dart';
+import 'package:beer_app/presentation/widgets/material_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+const searchBarHeight = 56.0;
+
+class SliverSearchBar extends StatefulWidget {
+  const SliverSearchBar({
+    super.key,
+    required this.title,
+    required this.onTap,
+  });
+
+  final String title;
+  final VoidCallback onTap;
+
+  @override
+  State<SliverSearchBar> createState() => _SliverSearchBarState();
+}
+
+class _SliverSearchBarState extends State<SliverSearchBar> {
+  @override
+  Widget build(BuildContext context) {
+    final topPadding = MediaQuery.of(context).padding.top;
+    return MediaQuery.removePadding(
+      context: context,
+      removeBottom: true,
+      child: SliverPersistentHeader(
+        pinned: true,
+        delegate: _SliverSearchBarDelegate(
+          topPadding: topPadding,
+          title: widget.title,
+          onTap: widget.onTap,
+        ),
+      ),
+    );
+  }
+}
+
+class _SliverSearchBarDelegate extends SliverPersistentHeaderDelegate {
+  const _SliverSearchBarDelegate({
+    required this.topPadding,
+    required this.title,
+    required this.onTap,
+  });
+
+  final double topPadding;
+  final String title;
+  final VoidCallback onTap;
+
+  @override
+  double get minExtent => topPadding + searchBarHeight;
+
+  @override
+  double get maxExtent => minExtent;
+
+  @override
+  bool shouldRebuild(covariant _SliverSearchBarDelegate oldDelegate) {
+    return topPadding != oldDelegate.topPadding;
+  }
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    return FlexibleSpaceBar.createSettings(
+      minExtent: minExtent,
+      maxExtent: maxExtent,
+      currentExtent: minExtent,
+      child: _FloatingHeader(
+        child: _SearchBar(
+          title: title,
+          onTap: onTap,
+        ),
+      ),
+    );
+  }
+}
+
+class _FloatingHeader extends StatefulWidget {
+  const _FloatingHeader({
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  State<_FloatingHeader> createState() => _FloatingHeaderState();
+}
+
+class _FloatingHeaderState extends State<_FloatingHeader> {
+  ScrollPosition? _position;
+  bool _visible = true;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _position?.removeListener(_scrollListener);
+    _position = Scrollable.of(context)?.position;
+    _position?.addListener(_scrollListener);
+  }
+
+  void _scrollListener() {
+    if (_position == null) return;
+    final visible = _position!.userScrollDirection == ScrollDirection.forward;
+    if (_visible != visible) {
+      setState(() => _visible = visible);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedFadeScale(
+      visible: _visible,
+      duration: shortAnimDuration,
+      child: widget.child,
+    );
+  }
+
+  @override
+  void dispose() {
+    _position?.removeListener(_scrollListener);
+    super.dispose();
+  }
+}
+
+class _SearchBar extends StatelessWidget {
+  const _SearchBar({
+    required this.title,
+    required this.onTap,
+  });
+
+  final String title;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: MaterialCard(
+            elevation: ElevationLevel.level3,
+            child: InkWell(
+              borderRadius: borderRadius,
+              onTap: onTap,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16, 12, 12, 12),
+                child: Row(
+                  children: [
+                    const Icon(AppIcons.search),
+                    const SizedBox(width: 16),
+                    Text(
+                      title,
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ test/presentation/widgets/sliver_search_bar_test.dart
@@ -0,0 +1,97 @@
+import 'package:beer_app/presentation/app_icons.dart';
+import 'package:beer_app/presentation/widgets/animated_fade_scale.dart';
+import 'package:beer_app/presentation/widgets/material_card.dart';
+import 'package:beer_app/presentation/widgets/sliver_search_bar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../utils/widget_tester_ext.dart';
+
+void main() {
+  group(
+    'SliverSearchBar',
+    () {
+      const title = 'Title';
+
+      var calls = 0;
+      void onTap() {
+        calls++;
+      }
+
+      setUp(() {
+        calls = 0;
+      });
+
+      Future<void> pumpWidget(WidgetTester tester) async {
+        await tester.pumpAppWidget(
+          CustomScrollView(
+            slivers: [
+              SliverSearchBar(
+                title: title,
+                onTap: onTap,
+              ),
+              const SliverToBoxAdapter(
+                child: SizedBox(
+                  height: 1000,
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      testWidgets(
+        'should show MaterialCard, search icon and title',
+        (tester) async {
+          // act
+          await pumpWidget(tester);
+
+          // assert
+          expect(find.byType(MaterialCard), findsOneWidget);
+          expect(find.byIcon(AppIcons.search), findsOneWidget);
+          expect(find.text(title), findsOneWidget);
+        },
+      );
+
+      testWidgets(
+        'should call onTap on tap',
+        (tester) async {
+          // act
+          await pumpWidget(tester);
+
+          await tester.tap(find.byType(MaterialCard));
+
+          // assert
+          expect(calls, 1);
+        },
+      );
+
+      testWidgets(
+        'should hide widget on scroll down and show on scroll up',
+        (tester) async {
+          // act
+          await pumpWidget(tester);
+
+          await tester.drag(find.byType(Scrollable), const Offset(0, -10));
+          await tester.pumpAndSettle();
+
+          // assert
+          final finder = find.ancestor(
+            of: find.byType(MaterialCard),
+            matching: find.byType(AnimatedFadeScale),
+          );
+          final animatedFadeScale = tester.widget<AnimatedFadeScale>(finder);
+          expect(animatedFadeScale.visible, false);
+
+          // act
+          await tester.drag(find.byType(Scrollable), const Offset(0, 1));
+          await tester.pumpAndSettle();
+
+          // assert
+          final animatedFadeScale2 = tester.widget<AnimatedFadeScale>(finder);
+          expect(animatedFadeScale2.visible, true);
+        },
+      );
+    },
+  );
+}
