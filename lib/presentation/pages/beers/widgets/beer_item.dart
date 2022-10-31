4e6d22d14779aced3db57eac7a523276bae6c5d2
Daniel Górka <daniel.gorka24@gmail.com>
2022-10-31 15:24:01 +0100

    Move OpenContainer down in the widget tree in BeerItem

--- lib/presentation/pages/beers/widgets/beer_item.dart
+++ lib/presentation/pages/beers/widgets/beer_item.dart
@@ -19,61 +19,61 @@ class BeerItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return OpenContainer(
-        tag: beer.id.value,
-        color: Colors.transparent,
-        clipBehavior: Clip.none,
-        shape: const RoundedRectangleBorder(borderRadius: borderRadius),
-        builder: (context) {
-          return InkWell(
-            borderRadius: borderRadius,
-            onTap: () {
-              context.router.push(
-                BeerDetailsRoute(
-                  beerId: beer.id.value,
-                  beer: beer,
-                ),
-              );
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Stack(
-                children: [
-                  Positioned.fill(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Expanded(
-                            child: BeerImageView(
-                              imageUrl: beer.imageUrl,
-                            ),
+    return InkWell(
+      borderRadius: borderRadius,
+      onTap: () {
+        context.router.push(
+          BeerDetailsRoute(
+            beerId: beer.id.value,
+            beer: beer,
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: OpenContainer(
+                tag: beer.id.value,
+                color: Colors.transparent,
+                shape: const RoundedRectangleBorder(borderRadius: borderRadius),
+                builder: (context) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Expanded(
+                          child: BeerImageView(
+                            imageUrl: beer.imageUrl,
                           ),
-                          const SizedBox(height: 8),
-                          Text(
-                            '${beer.name.value}\n\n',
-                            style: Theme.of(context).textTheme.titleMedium,
-                            textAlign: TextAlign.center,
-                            overflow: TextOverflow.fade,
-                            maxLines: 2,
-                          ),
-                        ],
-                      ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          '${beer.name.value}\n\n',
+                          style: Theme.of(context).textTheme.titleMedium,
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.fade,
+                          maxLines: 2,
+                        ),
+                      ],
                     ),
-                  ),
-                  Positioned(
-                    top: 0,
-                    right: 0,
-                    child: FavouriteBeerButton(
-                      favourite: beer.favourite,
-                      onFavouriteChanged: onFavouriteChanged,
-                    ),
-                  ),
-                ],
+                  );
+                },
               ),
             ),
-          );
-        });
+            Positioned(
+              top: 0,
+              right: 0,
+              child: FavouriteBeerButton(
+                favourite: beer.favourite,
+                onFavouriteChanged: onFavouriteChanged,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
