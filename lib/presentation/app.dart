c8d6465820447337f205c5d3dc06c378f0ae6458
Daniel Górka <daniel.gorka24@gmail.com>
2022-10-31 20:22:36 +0100

    Add custom SystemUiOverlayStyle

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -3,6 +3,7 @@ import 'package:beer_app/presentation/routes/router.dart';
 import 'package:beer_app/presentation/theme.dart';
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -26,6 +27,12 @@ class _AppState extends State<App> {
           darkTheme: AppTheme.dark(darkScheme),
           routeInformationParser: _router.defaultRouteParser(),
           routerDelegate: _router.delegate(),
+          builder: (context, child) {
+            return AnnotatedRegion<SystemUiOverlayStyle>(
+              value: AppTheme.overlay(Theme.of(context)),
+              child: child!,
+            );
+          },
         );
       },
     );
--- lib/presentation/theme.dart
+++ lib/presentation/theme.dart
@@ -1,5 +1,6 @@
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 abstract class AppTheme {
   static const Color beerColor = Color(0xFFC96E12);
@@ -36,6 +37,22 @@ abstract class AppTheme {
     return const AppBarTheme(
       centerTitle: true,
       elevation: 3,
+    );
+  }
+
+  static SystemUiOverlayStyle overlay(ThemeData theme) {
+    final isDark = theme.brightness == Brightness.dark;
+    return SystemUiOverlayStyle(
+      statusBarBrightness: isDark ? Brightness.dark : Brightness.light,
+      statusBarColor: Colors.transparent,
+      systemNavigationBarColor: ElevationOverlay.applySurfaceTint(
+        theme.colorScheme.surface,
+        theme.colorScheme.surfaceTint,
+        3,
+      ),
+      systemNavigationBarIconBrightness:
+          isDark ? Brightness.light : Brightness.dark,
+      statusBarIconBrightness: isDark ? Brightness.light : Brightness.dark,
     );
   }
 }
